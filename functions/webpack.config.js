6b0177a108284c7918520b7a5042a4acc760ef9d
Gabe Rodriguez <gabe@spotify.com>
2020-06-10 15:59:23 +0200

    Transpile/compile/bundling circus

--- functions/webpack.config.js
+++ functions/webpack.config.js
@@ -10,27 +10,26 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.ts?$/,
-        loader: 'ts-loader',
-        options: {
-          transpileOnly: true,
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-typescript'],
+              [
+                '@babel/preset-env',
+                {
+                  targets: {
+                    node: '10',
+                  },
+                  useBuiltIns: 'usage',
+                  corejs: 3,
+                },
+              ],
+            ],
+          },
         },
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name]-[sha512:hash:hex:5].[ext]',
-              publicPath: '/_next/static/files',
-              outputPath: 'static/files',
-            },
-          },
-          {
-            loader: 'image-webpack-loader',
-          },
-        ],
       },
     ],
   },
